249355e0aacb690504f29c5ed8817185a0fb6ef8
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2024-03-02 15:30:24 +0100

    Tolerate models with more than 8192 token without loosing all memory

--- evaluation/models/huggingface_backends/vllm_backend.py
+++ evaluation/models/huggingface_backends/vllm_backend.py
@@ -18,6 +18,7 @@ async def create_model(*, model_path, tokenizer_path, dtype):
             disable_log_requests=True,
             trust_remote_code=True,
             max_num_seqs=1024,
+            max_model_len=8192,
         ),
         start_engine_loop=False,
     )
